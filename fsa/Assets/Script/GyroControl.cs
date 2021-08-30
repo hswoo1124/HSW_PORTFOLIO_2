5089041148f2773b26ac74b7279720bc74f602a0
hswoo1124 <hswoo1124@naver.com>
2021-08-30 16:13:43 +0900

    2019 SW경진대회 참가작 FSA
    Food Search App
    대전시에서 제공되는 모범식당 오픈api를 이용하여 만든 식당 서치어플.
    사용자의 위치를 기반으로 원하는 모범식당을 찾아주고 안내해주며 ar기능으로 정보를 제공한다.

---
+++ fsa/Assets/Script/GyroControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroControl : MonoBehaviour
+{
+    private Rigidbody rigi;
+
+    private void Start()
+    {
+        rigi = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        rigi.AddForce(Input.acceleration*30);
+    }
+}
---
+++ fsa/Assets/Script/Rader.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class testgps : MonoBehaviour
+{
+    public string Position_X { get; set; }
+    public string Position_Y { get; set; }
+    public testgps(string x, string y)
+    {
+        Position_X = x;
+        Position_Y = y;
+    }
+}
+
+public class point : MonoBehaviour
+{
+    public float Position_X { get; set; }
+    public float Position_Y { get; set; }
+    public point(float x, float y)
+    {
+        Position_X = x;
+        Position_Y = y;
+    }
+}
+public class Rader : MonoBehaviour
+{
+    public Image my;
+    public double lon;
+    public double lat;
+    public GameObject raderpoint;
+    List<point> pt = new List<point>();
+    List<testgps> tg = new List<testgps>();
+    public Text testtext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lon = 127.445419;
+        lat = 36.33609;
+        float fx = my.rectTransform.position.x;
+        float fy = my.rectTransform.position.y;
+        testgps t = new testgps(127.391234.ToString(), 36.30123.ToString());
+        tg.Add(t);
+        testgps t1 = new testgps(127.374321.ToString(), 36.345685.ToString());
+        tg.Add(t1);
+        testgps t2 = new testgps(127.459874.ToString(), 36.31474.ToString());
+        tg.Add(t2);
+       
+            foreach (testgps t5 in tg)
+            {
+                point p = new point(float.Parse((lon - double.Parse(t5.Position_X)).ToString())*500, float.Parse((lat - double.Parse(t5.Position_Y)).ToString())*500);
+            testtext.text += p.Position_X.ToString() + "\n";
+            testtext.text += p.Position_Y.ToString() + "\n";
+            pt.Add(p);
+            }
+        
+    }
+    public void butc()
+    {
+        foreach(point point in pt)
+        {
+            GameObject g = Instantiate(raderpoint, transform.position + new Vector3(point.Position_X, point.Position_Y, 0), transform.rotation);
+            g.GetComponent<RectTransform>().localScale = new Vector3(0.12f, 0.1f, 0);
+        }
+
+    }
+}
